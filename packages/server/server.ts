d23640560716a830d1d95ca827316d31dc09cd5e
Theo Sandell <theo01sandell@gmail.com>
2022-05-04 15:36:06 +0200

    New project structure again

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-import apiRouter from './router/api';
-import webRouter from './router/web';
+import router from './router/api';
 import path from 'path';
 
 const app: express.Application = express();
@@ -12,14 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/game', express.static(path.join(__dirname, 'game')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use('/', webRouter);
-app.use('/api', apiRouter);
+app.use('/api', router);
 
 app.listen(process.env.PORT ?? 3000, () => {
   console.log(`Listening on port ${process.env.PORT ?? 3000}`);
--- src/router/web.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router: Router = Router();
-
-router.get('/', (req, res) => {
-  res.render('index');
-});
-
-export default router;
